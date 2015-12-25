93ccf6a76b87a4466b6b02ff75ae820944a6c2eb
Eisuke Sato <e.sato@livingstyle.co.jp>
2015-12-25 21:15:04 +0900

    Parseから取得したデータをtableViewに表示する処理を実装

--- jikoman/OtherGoodTableViewController.m
+++ jikoman/OtherGoodTableViewController.m
@@ -8,19 +8,16 @@
 
 #import "OtherGoodTableViewController.h"
 
+#import <Parse.h>
+
 @interface OtherGoodTableViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (strong, nonatomic) IBOutlet UITableView *otherTableView;
-@property (strong ,nonatomic) NSArray *imgArray;
-@property (strong, nonatomic) NSArray *label2Array;
-
+@property (nonatomic) NSMutableArray *items;
+//@property (strong, nonatomic) NSArray *label2Array;
 
 @end
 
 @implementation OtherGoodTableViewController
-
-
-
-
 
 - (IBAction)unwindtoSubmit:(UIStoryboardSegue*)segue
 {
@@ -30,22 +27,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-//    デリゲートメソッドをこのクラスで実装する
-//    self.otherTableView.delegate = self;
-//    self.otherTableView.dataSource = self;
-    
 //    ひとまず８このデータの配列を置いとく
     
-    self.imgArray = @[@"img0.JPG",@"img1.JPG",@"img2.JPG",@"img3.JPG",
-                 @"img4.JPG",@"img5.JPG",@"img6.JPG",@"img7.JPG"];
+//    self.label2Array = @[@"2013/8/23/16:04",@"2013/8/23/16:15",@"2013/8/23/16:47",@"2013/8/23/17:10",
+//                    @"2013/8/23/1715:",@"2013/8/23/17:21",@"2013/8/23/17:33",@"2013/8/23/17:41"];
     
-    self.label2Array = @[@"2013/8/23/16:04",@"2013/8/23/16:15",@"2013/8/23/16:47",@"2013/8/23/17:10",
-                    @"2013/8/23/1715:",@"2013/8/23/17:21",@"2013/8/23/17:33",@"2013/8/23/17:41"];
-    
-//    [self.tableView registerClass:[UITableViewCell class]
-//           forCellReuseIdentifier:@"Cell"];
     
     [self.refreshControl addTarget:self action:@selector(updateCells) forControlEvents:UIControlEventValueChanged];
+    [self loadItems];
+}
+
+- (void)loadItems {
+    PFQuery *query = [PFQuery queryWithClassName:@"Ichizen"];
+    [query orderByAscending:@"createdAt"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+        [objects enumerateObjectsUsingBlock:^(PFObject *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            [self.items addObject:obj];
+        }];
+        [self.refreshControl endRefreshing];
+        [self.tableView reloadData];
+    }];
 }
 
 - (void)updateCells{
@@ -68,40 +69,51 @@
 //１つのセクションに返すセルの数
 - (NSInteger)tableView:(UITableView *)tableView
  numberOfRowsInSection:(NSInteger)section {
-    return self.imgArray.count;
+    return self.items.count;
 }
 
-
-
 //テーブルに表示するセルを返す
-//
-//
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
 //    再利用できるセルがあれば再利用する
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"
                                                             forIndexPath:indexPath];
-////    再利用できないセルがなければ新規で生成する
-//    if (cell == nil) {
-//        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault
-//                                     reuseIdentifier:cellIdentifer];
-//    }
+    PFObject *ichizen = self.items[indexPath.row];
     
-    UILabel *label1 = [cell viewWithTag:1];
-    label1.text = self.label2Array[indexPath.row];
+//    UILabel *label1 = [cell viewWithTag:1];
+//    label1.text = self.label2Array[indexPath.row];
     
     UILabel *label2 = [cell viewWithTag:2];
-    label2.text = [NSString stringWithFormat:@"No.%d", (int)(indexPath.row + 1)];
+    label2.text = ichizen[@"text"];
     
-    UIImage *img = [UIImage imageNamed:self.imgArray[indexPath.row]];
     UIImageView *imgView = (UIImageView*)[cell viewWithTag:3];
-    imgView.image = img;
+    PFFile *imageFile = ichizen[@"Img1"];
+    if (imageFile) {
+        [imageFile getDataInBackgroundWithBlock:^(NSData * _Nullable data, NSError * _Nullable error) {
+            if (!error) {
+                imgView.image = [UIImage imageWithData:data];
+            } else {
+                NSLog(@"%@", error);
+                imgView.image = nil;
+                imgView.backgroundColor = [UIColor darkGrayColor];
+            }
+        }];
+    } else {
+        imgView.image = nil;
+        imgView.backgroundColor = [UIColor darkGrayColor];
+    }
     
 //    cell.textLabel.text = [[NSString alloc]initWithFormat:@"%ld行目のセル", indexPath.row + 1];
     
     return cell;
 }
 
+- (NSMutableArray *)items {
+    if (!_items) {
+        _items = @[].mutableCopy;
+    }
+    return _items;
+}
 
 @end
